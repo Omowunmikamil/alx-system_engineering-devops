86fcd41205ca2bdde2dee566bdfca5b57fbce78d
Omowunmijuin <balikiskamil@gmail.com>
2023-08-10 00:07:38 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that returns the numbers of top 10 hot post listed for a given subreddit.
+Script that returns the numbers of top 10 hot 
+post listed for a given subreddit.
 """
 
 import requests
@@ -13,7 +14,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     query_string = {"limit" : 10}
 
-    get_api = requst.get(url, headers=header, params=query_string, allow_redirects=False)
+    get_api = requst.get(url, headers=header, allow_redirects=False)
 
     if get_api.status.code == 200:
         data = get_api.json()
