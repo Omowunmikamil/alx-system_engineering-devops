3534d466540f832baea1bc31c0ee33c82040cf1c
Omowunmijuin <balikiskamil@gmail.com>
2023-08-10 23:35:57 +0000

    syntax error crrection

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-Script that returns the numbers of top 10 hot 
+Script that returns the numbers of top 10 hoit
 post listed for a given subreddit.
 """
 
 import requests
 
+
 def top_ten(subreddit):
     """Function that returns the numbers of
     subscribers of a subreddit passed to it"""
 
-    header = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    get_api = requst.get(url, headers=header, allow_redirects=False)
+    get_api = requsts.get(url, headers=headers, allow_redirects=False)
 
-    if get_api.status.code == 200:
+    if get_api.status_code == 200:
         data = get_api.json()
         top_hots = data.get("data").get("children")
         for hot_topics in top_hots:
