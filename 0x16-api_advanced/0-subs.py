f4e7c27605f273713a10f4b2f798dc2e43aa8c95
Omowunmijuin <balikiskamil@gmail.com>
2023-08-10 00:11:38 +0000

    Correct url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,10 @@ def number_of_subscribers(subreddit):
     subscribers of a subreddit passed to it"""
 
     headers = {"User-Agent" : "mozilla/5.0"}
-    api_url = "https://www.redit.com/r/{}/about.json".format(subreddit)
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code == 200:
         return response.json().get("data").get("subscribers")
-    return (0)
+    else:
+        return (0)
